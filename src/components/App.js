06ab735513809758b784669d6293ed4249d49e18
Volodymyr <vlrohmanov@gmail.com>
2023-01-03 12:50:30 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,64 +1,59 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 // import PropTypes from 'prop-types';
 import { Section } from './section/Section';
 import { FeedbackOptions } from './feedbackoptions/FeedbackOptions';
 import { Statistics } from './statistics/Statistics';
 import css from '../components/section/Section.module.css';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGoodFeedback] = useState(0);
+  const [neutral, setNeutralFeedback] = useState(0);
+  const [bad, setBadFeedback] = useState(0);
+  const options = { good, neutral, bad };
+
+  const onLeaveFeedback = event => {
+    console.log(event.target.name);
+    switch (event.target.name) {
+      case 'good':
+        setGoodFeedback(good + 1);
+        break;
+
+      case 'neutral':
+        setNeutralFeedback(neutral + 1);
+        break;
+
+      case 'bad':
+        setBadFeedback(bad + 1);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  // onLeaveFeedback = event => {
-  //   if (event.target.name === 'good') {
-  //     this.setState(prevState => ({ good: prevState.good + 1 }));
-  //   } else if (event.target.name === 'neutral') {
-  //     this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
-  //   } else if (event.target.name === 'bad') {
-  //     this.setState(prevState => ({ bad: prevState.bad + 1 }));
-  //   }
-  // };
-
-  onLeaveFeedback = event => {
-    this.setState(prevState => ({
-      [event.target.name]: prevState[event.target.name] + 1,
-    }));
-  };
-
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
-  }
+  };
 
-  countPositiveFeedbackPercentage() {
-    const { good } = this.state;
-    return ((good / this.countTotalFeedback()) * 100).toFixed(0);
-  }
+  const countPositiveFeedbackPercentage = () => {
+    return ((good / countTotalFeedback()) * 100).toFixed(0);
+  };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <div className={css.wrapper}>
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions
-            options={this.state}
-            onChange={this.onLeaveFeedback}
-          />
-        </Section>
+  return (
+    <div className={css.wrapper}>
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions options={options} onChange={onLeaveFeedback} />
+      </Section>
 
-        <Section title={'Statistics'}>
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
-        </Section>
-      </div>
-    );
-  }
-}
+      <Section title={'Statistics'}>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+      </Section>
+    </div>
+  );
+};
